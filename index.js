e8388a757e2b158e131ce6aa278a57948cb24443
Jerscape <jeremydutton5@gmail.com>
2023-07-03 08:13:24 -0500

    final commit

--- index.js
+++ index.js
@@ -6,24 +6,34 @@ function newTask(title, description) {
   const task = {
     title: title,
     description: description,
-    complete: false
+    complete: false,
+
+
+    logState: function(){
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`)
+    },
+
+    markCompleted: function(){
+      this.complete = true;
+    }
+
   }
 
-  return task
+  return task;
 }
 
 // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(task) {
-  task.complete = true
-  //taskComplete[taskIndex] = true;
-}
+// function completeTask(task) {
+//   task.complete = true
+//   //taskComplete[taskIndex] = true;
+// }
 
-// // Print the state of a task to the console in a nice readable way
-function logTaskState(task) {
-  //const title = taskTitles[taskIndex];
-  //const complete = taskComplete[taskIndex];
-  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
-}
+// // // Print the state of a task to the console in a nice readable way
+// function logTaskState(task) {
+//   //const title = taskTitles[taskIndex];
+//   //const complete = taskComplete[taskIndex];
+//   console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
+// }
 
 // // DRIVER CODE BELOW
 
@@ -31,7 +41,8 @@ const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter b
 const task2 = newTask("Do Laundry", "😨"); // task 1
 const tasks = [task1, task2]
 
-console.log(tasks)
-logTaskState(task1); // Clean Cat Litter has not been completed
-completeTask(task1);
-logTaskState(task1); // Clean Cat Litter has been completed
+task1.logState() // Clean Cat Litter has not been completed
+task1.markCompleted()
+task1.logState() // Clean Cat Litter has been completed
+
+
